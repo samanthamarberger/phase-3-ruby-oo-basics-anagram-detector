fd773a77c3c203da2b3fb5e90159ea7a42426ca4
samanthamarberger <slatham04@gmail.com>
2023-07-07 16:28:29 -0600

    DONE

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        matches = []
+        array.filter do |i|
+            if i.chars.sort == word.chars.sort
+                matches << i
+            end
+        end
+        matches
+    end
+
+end
